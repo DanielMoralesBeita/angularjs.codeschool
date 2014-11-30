455c3e24b46fccbb80f726d05e1978bf619f399f
akaramires <e.abdurayimov@gmail.com>
2014-11-30 20:18:55 +0300

    3.6 Creating Review Controller

--- js/app.js
+++ js/app.js
@@ -22,6 +22,15 @@
         };
     });
 
+    app.controller('ReviewController', function () {
+        this.review = {};
+
+        this.addReview = function (product) {
+            product.reviews.push(this.review);
+            this.review = {};
+        };
+    });
+
     app.controller('GalleryController', function () {
         this.current = 0;
         this.setCurrent = function (newGallery) {
